6e4022246c40a06fe33eef090078cc9b926a6fee
C0ldHands <48500351+C0ldHands@users.noreply.github.com>
2021-01-04 20:53:34 -0500

    Added validation

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const MOVIES = require('./movies-data.json')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
+function validateBearerToken(req,res,next){
+  const authToken = req.get('Authorization')
+  const apiToken = process.env.API_TOKEN
+
+  if(!authToken || authToken.split(' ')[1] !== apiToken){
+    return res.status(401).json({error: 'Unauthorized request'})
+  }
+
+  next()
+}
+
+app.use(validateBearerToken)
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
